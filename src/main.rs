5af71ef7fbe763f2cd9700efaa18db59781ece06
Andreas H. Klostergaard <akl@klostergaard.net>
2020-01-02 14:52:49 +0100

    adding repl history

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,13 @@ fn interactive() {
     let mut rl = Editor::<()>::new();
     let mut env = stdlib::stdlib();
 
-    loop {
+    'repl: loop {
         let readline = rl.readline(">> ");
+        let mut line;
         match readline {
             Ok(l) => {
-                let mut line;
                 line = l.clone();
-                'repl: loop {
+                loop {
                     match eval(line.clone(), &mut env) {
                         Ok(result) => {
                             println!("{}", result);
@@ -35,6 +35,7 @@ fn interactive() {
                 break;
             }
         }
+        rl.add_history_entry(line);
     }
 }
 
@@ -51,42 +52,42 @@ fn eval(input: String, env: &mut stdlib::Environment) -> Result<String, parser::
 }
 
 fn main() {
-    let test = "(test '(\"cool \\\"string\\\" stuff\" 123))";
-    println!("String to test:\n{}\n", test);
-    let mut lexed = lexer::lex(test).unwrap();
-    println!("Lexer output:\n{:?}\n", lexed);
-    let parsed = parser::parse(&mut lexed);
-    println!("Parser output:\n{:?}\n", parsed);
+    //let test = "(test '(\"cool \\\"string\\\" stuff\" 123))";
+    //println!("String to test:\n{}\n", test);
+    //let mut lexed = lexer::lex(test).unwrap();
+    //println!("Lexer output:\n{:?}\n", lexed);
+    //let parsed = parser::parse(&mut lexed);
+    //println!("Parser output:\n{:?}\n", parsed);
 
-    let converted = convert::convert(parsed.expect("Could not parse the input"));
-    println!("Converter output:\n{:?}\n", converted);
+    //let converted = convert::convert(parsed.expect("Could not parse the input"));
+    //println!("Converter output:\n{:?}\n", converted);
 
-    println!("Converted output in pretty print:");
-    for i in converted.iter() {
-        println!("{}", i);
-    }
+    //println!("Converted output in pretty print:");
+    //for i in converted.iter() {
+    //    println!("{}", i);
+    //}
 
-    println!("\n\n\n");
+    //println!("\n\n\n");
 
-    //let program = "((lambda (x y) x y) 5 8)";
-    //let program = "((lambda (x) (+ x x)) 5)";
-    //let program = "(eval ((lambda (x) (+ x x)) 5))";
-    //let program = "'((lambda (x) (+ x x)) 5)";
-    let program = "(eval '((lambda (x) (+ x x)) 5))";
-    //let program = "(quote true)";
-    //let program = "(lambda (x) x)";
-    println!("Program:\n{}", program);
-    let data = convert::convert(
-        parser::parse(&mut lexer::lex(program).unwrap()).expect("Could not parse the input"),
-    );
-    println!("Converted data:\n{}", data.first().unwrap());
-    let answer = stdlib::eval(&data.first().unwrap(), &mut stdlib::stdlib())
-        .expect("Could not evaluate the input");
-    if let stdlib::EnvItem::Data(a) = answer {
-        println!("Answer:\n{}", a);
-    } else {
-        println!("Answer (not pretty):\n{:?}", answer);
-    }
+    ////let program = "((lambda (x y) x y) 5 8)";
+    ////let program = "((lambda (x) (+ x x)) 5)";
+    ////let program = "(eval ((lambda (x) (+ x x)) 5))";
+    ////let program = "'((lambda (x) (+ x x)) 5)";
+    //let program = "(eval '((lambda (x) (+ x x)) 5))";
+    ////let program = "(quote true)";
+    ////let program = "(lambda (x) x)";
+    //println!("Program:\n{}", program);
+    //let data = convert::convert(
+    //    parser::parse(&mut lexer::lex(program).unwrap()).expect("Could not parse the input"),
+    //);
+    //println!("Converted data:\n{}", data.first().unwrap());
+    //let answer = stdlib::eval(&data.first().unwrap(), &mut stdlib::stdlib())
+    //    .expect("Could not evaluate the input");
+    //if let stdlib::EnvItem::Data(a) = answer {
+    //    println!("Answer:\n{}", a);
+    //} else {
+    //    println!("Answer (not pretty):\n{:?}", answer);
+    //}
 
     interactive();
 }
